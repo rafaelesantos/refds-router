596319a47312c62d8e65e881284cb2d7da3e427e
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-04-11 14:00:00 -0400

    Update RefdsRoutingView.swift

--- Sources/RefdsRouter/RefdsRoutingView.swift
+++ Sources/RefdsRouter/RefdsRoutingView.swift
@@ -4,13 +4,14 @@ public struct RefdsRoutingView<
     Content: View,
     Destination: RefdsRoutable
 >: View {
-    @StateObject var router: RefdsRouter<Destination> = .init(isPresented: .constant(.none))
+    @StateObject var router: RefdsRouter<Destination>
     private let content: (RefdsRouter<Destination>) -> Content
     
     public init(
-        type: Destination.Type,
+        router: RefdsRouter<Destination>,
         @ViewBuilder content: @escaping (RefdsRouter<Destination>) -> Content
     ) {
+        self._router = StateObject(wrappedValue: router)
         self.content = content
     }
     
